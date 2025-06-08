da14c4f9943a137c8e900fb0bd1d34ef49c9d4ab
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-06-07 23:34:59 -0500

    se agrega la funcionalidad de cargar en el inicio del juego donde en el caso de la GUI se usa jchoosefiles para que el selecciona varios archivos guardados serializados cuando los selecciona se verifica que todo sea valido y que halla seleccionado despues se prosede a deserializar ya hacer un casting an entrenador para continuar con la seleccion de los pokemones

--- src/Vistas/InicioPokemonTerminal.java
+++ src/Vistas/InicioPokemonTerminal.java
@@ -1,5 +1,6 @@
 package Vistas;
 
+import java.io.File;
 import java.util.Scanner;
 
 import Controladores.ControladorInicio;
@@ -28,6 +29,7 @@ public class InicioPokemonTerminal implements Inicio{
         System.out.println("Eliga una opcion:");
         System.out.println("1. Iniciar Juego");
         System.out.println("2. Cambiar a GUI");
+        System.out.println("3. Cargar Entrenadores");
         int opcion = scanner.nextInt();
         switch (opcion) {
             case 1:
@@ -37,6 +39,23 @@ public class InicioPokemonTerminal implements Inicio{
         
             case 2:
                 controlador.cambiarVista();
+                break;
+            case 3:
+                controlador.setESGUI(false);
+                File carpeta = new File("src/Guardados");
+                File[] archivos_enteros = carpeta.listFiles();
+                File[] archivos = new File[2];
+                for (int i = 0; i < archivos_enteros.length; i++) {
+                    System.out.println((i + 1) + ". " + archivos_enteros[i].getName());
+                }
+                System.out.println("Seleccione dos archivos para cargar los entrenadores");
+                System.out.println("Ingrese el numero del primer archivo:");
+                int primerArchivo = scanner.nextInt() - 1;
+                System.out.println("Ingrese el numero del segundo archivo:");
+                int segundoArchivo = scanner.nextInt() - 1;
+                archivos [0] = archivos_enteros[primerArchivo];
+                archivos [1] = archivos_enteros[segundoArchivo];
+                controlador.IniciarCarga(archivos);
                 break;
         }
         
