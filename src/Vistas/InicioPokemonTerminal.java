6cb7fe762450f3d89585227e661bf36151cf5f8d
JuanManuel26706 <juan.m.gomez.a@correounivalle.edu.co>
2025-06-10 15:52:26 -0500

    Se empiezan a agregar las excepciones en funcionamiento de terminal, falta completar algunos puntos

--- src/Vistas/InicioPokemonTerminal.java
+++ src/Vistas/InicioPokemonTerminal.java
@@ -1,9 +1,11 @@
 package Vistas;
 
 import java.io.File;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Controladores.ControladorInicio;
+import Excepciones.ExcepcionDeRangoIndice;
 import Interfaces.Inicio;
 
 public class InicioPokemonTerminal implements Inicio{
@@ -25,18 +27,18 @@ public class InicioPokemonTerminal implements Inicio{
 
     @Override
     public void Iniciar() {
-        System.out.println("Bienvenido al juego de Pokemon Java!");
-        System.out.println("Eliga una opcion:");
-        System.out.println("1. Iniciar Juego");
-        System.out.println("2. Cambiar a GUI");
-        System.out.println("3. Cargar Entrenadores");
-        int opcion = scanner.nextInt();
-        switch (opcion) {
+        try {
+            System.out.println("Bienvenido al juego de Pokemon Java!");
+            System.out.println("Eliga una opción:");
+            System.out.println("1. Iniciar Juego");
+            System.out.println("2. Cambiar a GUI");
+            System.out.println("3. Cargar Entrenadores");
+            int opcion = scanner.nextInt();
+            switch (opcion) {
             case 1:
                 controlador.setESGUI(false);
                 controlador.iniciarJuego();
                 break;
-        
             case 2:
                 controlador.cambiarVista();
                 break;
@@ -57,7 +59,31 @@ public class InicioPokemonTerminal implements Inicio{
                 archivos [1] = archivos_enteros[segundoArchivo];
                 controlador.IniciarCarga(archivos);
                 break;
+            default:
+                throw new ExcepcionDeRangoIndice("Ingrese una de las opciones disponibles.");
+            }
         }
+        catch (InputMismatchException e) {
+            System.out.println("No se permiten caracteres, ingrese un número de las opciones válidas.");
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar();
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Archivo no encontrado.");
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar();
+        }
+        catch (ExcepcionDeRangoIndice e) {
+            System.out.println("Error: " + e.getMessage());
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar();
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar();
+        }
+        
         
         
         
--- src/Vistas/SeleccionPokemonTerminal.java
+++ src/Vistas/SeleccionPokemonTerminal.java
@@ -1,9 +1,11 @@
 package Vistas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Clases.Pokemon;
 import Controladores.ControladorSeleccion;
+import Excepciones.ExcepcionDeRangoIndice;
 import Interfaces.SeleccionPokemonInterface;
 
 public class SeleccionPokemonTerminal implements SeleccionPokemonInterface{
@@ -21,41 +23,63 @@ public class SeleccionPokemonTerminal implements SeleccionPokemonInterface{
     }
 
     @Override
-    public void Iniciar(String nombreEntrenador1, String nombreEntrenador2, Pokemon[] pokemonesEntrenador1,
-            Pokemon[] pokemonesEntrenador2) {
-        while(true){
-            System.out.println("Seleccione una opcion:");
-            System.out.println("1. Seleccionar pokemones");
-            System.out.println("2. Cambiar a GUI");
-            System.out.println("3.Guardar");
-            int opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1:
-                    controlador.setESGUI(false);
-                    System.out.println("Entrenador 1: " + nombreEntrenador1);
-                    for (int i = 0; i < pokemonesEntrenador1.length; i++) {
-                        System.out.println("Pokemon " + (i + 1) + ": " + pokemonesEntrenador1[i].getNombre());
-                    }
-                    System.out.println("Selecciona un pokemon para iniciar la batalla:");
-                    int pokemonSeleccionado1 = scanner.nextInt();
-                    System.out.println("Entrenador 2: " + nombreEntrenador2);
-                    for (int i = 0; i < pokemonesEntrenador2.length; i++) {
-                        System.out.println("Pokemon " + (i + 1) + ": " + pokemonesEntrenador2[i].getNombre());
-                    }
-                    System.out.println("Selecciona un pokemon para iniciar la batalla:");
-                    int pokemonSeleccionado2 = scanner.nextInt();
-                    controlador.InicioBatalla(pokemonesEntrenador1[pokemonSeleccionado1 - 1], pokemonesEntrenador2[pokemonSeleccionado2 - 1]);
-                    return; // Salir del bucle una vez que se inicia la batalla
-                case 2:
-                    controlador.cambiarVista();
-                    return; // Salir del bucle una vez que se cambia a GUI
-                case 3:
-                    controlador.IniciarGuardado();
-                    break;
-                default:
-                    System.out.println("Opcion no valida");
+    public void Iniciar(String nombreEntrenador1, String nombreEntrenador2, Pokemon[] pokemonesEntrenador1, Pokemon[] pokemonesEntrenador2) {
+        try {
+            while(true){
+                System.out.println("Seleccione una opcion:");
+                System.out.println("1. Seleccionar pokemones");
+                System.out.println("2. Cambiar a GUI");
+                System.out.println("3.Guardar");
+                int opcion = scanner.nextInt();
+                switch (opcion) {
+                    case 1:
+                        controlador.setESGUI(false);
+                        System.out.println("Entrenador 1: " + nombreEntrenador1);
+                        for (int i = 0; i < pokemonesEntrenador1.length; i++) {
+                            System.out.println("Pokemon " + (i + 1) + ": " + pokemonesEntrenador1[i].getNombre());
+                        }
+                        System.out.println("Selecciona un pokemon para iniciar la batalla:");
+                        int pokemonSeleccionado1 = scanner.nextInt();
+                        System.out.println("Entrenador 2: " + nombreEntrenador2);
+                        for (int i = 0; i < pokemonesEntrenador2.length; i++) {
+                            System.out.println("Pokemon " + (i + 1) + ": " + pokemonesEntrenador2[i].getNombre());
+                        }
+                        System.out.println("Selecciona un pokemon para iniciar la batalla:");
+                        int pokemonSeleccionado2 = scanner.nextInt();
+                        controlador.InicioBatalla(pokemonesEntrenador1[pokemonSeleccionado1 - 1], pokemonesEntrenador2[pokemonSeleccionado2 - 1]);
+                        return; // Salir del bucle una vez que se inicia la batalla
+                    case 2:
+                        controlador.cambiarVista();
+                        return; // Salir del bucle una vez que se cambia a GUI
+                    case 3:
+                        controlador.IniciarGuardado();
+                        break;
+                    default:
+                        throw new ExcepcionDeRangoIndice("Opción no válida. Por favor, elija una opción entre 1 y 3.");
             }
         }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("No se permiten caracteres, ingrese un número de las opciones válidas.");
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar(nombreEntrenador1,nombreEntrenador2,pokemonesEntrenador1,pokemonesEntrenador2);
+        }
+        catch (ExcepcionDeRangoIndice e) {
+            System.out.println("Error: " + e.getMessage());
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar(nombreEntrenador1,nombreEntrenador2,pokemonesEntrenador1,pokemonesEntrenador2);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Selección de Pokémon fuera de rango.");
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar(nombreEntrenador1,nombreEntrenador2,pokemonesEntrenador1,pokemonesEntrenador2);
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            scanner.nextLine(); // Limpiar el buffer del scanner
+            Iniciar(nombreEntrenador1,nombreEntrenador2,pokemonesEntrenador1,pokemonesEntrenador2);
+        }
+        
     }
 
     @Override
